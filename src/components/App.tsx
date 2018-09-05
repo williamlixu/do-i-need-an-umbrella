18506ec3f18a21ea09e60f4a104ffe1347842f48
William <williamlixu@gmail.com>
2018-09-05 21:56:22 +1200

    base project

---
+++ src/components/App.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import '../styles/App.css';
+
+class App extends React.Component {
+  public render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Weather App Base Project</h1>
+        </header>
+        <p>
+          Hopefully one day the weather will show up here :)
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
