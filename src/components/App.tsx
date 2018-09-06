0235bc23797ce23b1fae345a8957aa67730e4d64
William <williamlixu@gmail.com>
2018-09-06 20:43:10 +1200

    added basic weather api call

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,15 +1,32 @@
+// tslint:disable:no-console
 import * as React from 'react';
 import '../styles/styles.css';
 import Header from './Header';
 import Search from './Search';
 import WeatherDisplay from './WeatherDisplay';
 
+const API_KEY = "a7ab45710f91f90bdb525f3825f7db7f";
+
 class App extends React.Component {
+
+  public getWeather = async (event: Event) => {
+    event.preventDefault();
+    
+    // construct API call from the information we got
+    const city : string =  'Auckland'; // should get from form
+    const API_CALL : string = `http://api.openweathermap.org/data/2.5/forecast?q=${city}&APPID=${API_KEY}`;
+    console.log(API_CALL);
+    // get weather data
+    const call = await fetch(API_CALL);
+    const data : JSON = await call.json();
+    console.log(data);
+  }
+
   public render() {
     return (
       <div>
         <Header />
-        <Search />
+        <Search getWeather={this.getWeather}/>
         <WeatherDisplay />
       </div>
     );
--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,43 +1,20 @@
-// tslint:disable:no-console
-// import Button from '@material-ui/core/Button';
-// import Input from '@material-ui/core/Input';
 import * as React from "react";
 
-export default class Search extends React.Component<{}> {
-    public handleSubmit(e: any) {
-        console.log("PRESSED");
-        e.preventDefault();
-    }
+interface IProps {
+    getWeather : any;
+}
 
-    // public render() {
-    //     return (
-    //         <div className="question">
-    //             <form onSubmit={this.handleSubmit}>
-    //                 <label>
-    //                     Do I need an umbrella in
-    //                     <input
-    //                         type = "text"
-    //                         className="lineInput"
-    //                         placeholder="Auckland"
-    //                     />
-    //                 </label>
-    //                 <div className="questionButton">
-    //                     <p><button>Well, do I?</button></p>
-    //                 </div>
-    //             </form>
-    //         </div>
-    //     );
-    // }
-
+export default class Search extends React.Component<IProps,{}> {
     public render() {
         return (
             <div className="question">
-                <form onSubmit={this.handleSubmit}>
+                <form onSubmit={this.props.getWeather}>
                     <label>
                         Do I need an umbrella in
                         <input
                             className="lineInput"
                             placeholder="Auckland"
+                            name="city"
                         />
                     </label>
                     <div className="questionButton">
